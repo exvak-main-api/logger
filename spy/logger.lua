47942122ae65276abd89d3b8f74d0131ea0dcce5
exvak-main-api <sigmagang2025@gmail.com>
2026-08-06 23:51:37 +0100

    Update logger.lua

--- spy/logger.lua
+++ spy/logger.lua
@@ -1,14 +1,33 @@
-local Logger = {}
+local Logger = {
+    entries = {}
+}
 
-Logger.logs = {}
+function Logger:add(kind, expression)
+    local entry = {
+        kind = kind,
+        expression = expression,
+    }
 
-function Logger:log(text)
-    print(text)
-    table.insert(self.logs, text)
+    table.insert(self.entries, entry)
+
+    print(("[%s] %s"):format(kind, expression))
+end
+
+function Logger:clear()
+    table.clear(self.entries)
 end
 
 function Logger:dump()
-    return table.concat(self.logs, "\n")
+    local result = {}
+
+    for _, entry in ipairs(self.entries) do
+        table.insert(
+            result,
+            ("[%s] %s"):format(entry.kind, entry.expression)
+        )
+    end
+
+    return table.concat(result, "\n")
 end
 
 return Logger
