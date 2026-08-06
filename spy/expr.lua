b62b5347b388269d3c56c24c0e1f19dff7a47903
exvak-main-api <sigmagang2025@gmail.com>
2026-08-07 00:38:00 +0100

    Update expr.lua

--- spy/expr.lua
+++ spy/expr.lua
@@ -2,6 +2,7 @@
 local Expr = {}
 
 local exprs = setmetatable({}, { __mode = "k" })
+local meta = setmetatable({}, { __mode = "k" })
 
 function Expr.set(proxy, expr)
     exprs[proxy] = expr
@@ -9,6 +10,14 @@ end
 
 function Expr.get(proxy)
     return exprs[proxy]
+end
+
+function Expr.setMeta(proxy, data)
+    meta[proxy] = data
+end
+
+function Expr.getMeta(proxy)
+    return meta[proxy]
 end
 
 return Expr
