e028ccac3673274b05df9203fb99b42b12ee2c64
exvak-main-api <sigmagang2025@gmail.com>
2026-08-06 23:52:11 +0100

    Update sandbox.lua

--- spy/sandbox.lua
+++ spy/sandbox.lua
@@ -1,10 +1,44 @@
-local proxy = require(script.Parent.proxy)
+local Proxy = require("./proxy")
+local Logger = require("./logger")
 
-local env = {
-    game = proxy("game"),
-    workspace = proxy("workspace"),
-    print = print,
-    warn = warn,
-}
+local Sandbox = {}
 
-return env
+function Sandbox.create()
+    local env = {}
+
+    env.game = Proxy.new("game")
+    env.workspace = Proxy.new("workspace")
+
+    env.print = function(...)
+        local args = table.pack(...)
+        local result = {}
+
+        for i = 1, args.n do
+            result[i] = Proxy.stringify(args[i])
+        end
+
+        Logger:add(
+            "PRINT",
+            "print(" .. table.concat(result, ", ") .. ")"
+        )
+    end
+
+    env.warn = env.print
+
+    env.getgenv = function()
+        return Proxy.new("getgenv()")
+    end
+
+    env.identifyexecutor = function()
+        Logger:add(
+            "CALL",
+            "identifyexecutor()"
+        )
+
+        return "Tracer", "1.0"
+    end
+
+    return env
+end
+
+return Sandbox
