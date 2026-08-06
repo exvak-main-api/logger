d0660f131203697ed9c42323b39716ad568ad81f
exvak-main-api <sigmagang2025@gmail.com>
2026-08-07 00:20:38 +0100

    Update proxy.lua

--- spy/proxy.lua
+++ spy/proxy.lua
@@ -1,16 +1,27 @@
-local Logger = require("./logger")
+local Logger   = require("./logger")
+local Expr     = require("./expr")
+local Compiler = require("./compiler")
 
 local Proxy = {}
 
-local function stringify(value)
-    if type(value) == "string" then
-        return string.format("%q", value)
-    end
+local mt
 
-    return tostring(value)
+local function makeLiteral(value)
+    return {
+        type = "literal",
+        value = value
+    }
 end
 
-local mt
+local function toExpr(value)
+    if type(value) == "table" then
+        local e = Expr.get(value)
+        if e then
+            return e
+        end
+    end
+    return makeLiteral(value)
+end
 
 mt = {
     __index = function(self, key)
@@ -19,94 +30,137 @@ mt = {
         local expr = {
             type = "index",
             base = base,
-            key = tostring(key)
+            key  = tostring(key)
         }
 
         local new = {}
-
         Expr.set(new, expr)
-
         return setmetatable(new, mt)
     end,
 
     __newindex = function(self, key, value)
         local base = Expr.get(self)
-        local expression =
-            (base.name or tostring(base))
-            .. "["
-            .. stringify(key)
-            .. "] = "
-            .. stringify(value)
 
-        Logger:add("WRITE", expression)
+        local target = {
+            type = "index",
+            base = base,
+            key  = tostring(key)
+        }
+
+        local assign = {
+            type  = "assign",
+            target = target,
+            value  = toExpr(value)
+        }
+
+        Logger:add(Compiler.compile(assign))
     end,
 
     __call = function(self, ...)
         local expr = Expr.get(self)
+        local args = {}
+
+        for i, v in ipairs({...}) do
+            args[i] = toExpr(v)
+        end
+
+        local newExpr
 
         if expr.type == "index" then
-            local args = {}
-
-            for i, v in ipairs({...}) do
-                args[i] = {
-                    type = "literal",
-                    value = v
-                }
-            end
-
-            local newExpr = {
+            -- method call: base:name(...)
+            newExpr = {
                 type = "method",
                 base = expr.base,
                 name = expr.key,
                 args = args
             }
-
-            Logger:add(
-                "CALL",
-                Compiler.compile(newExpr)
-            )
-
-            local proxy = {}
-
-            Expr.set(proxy, newExpr)
-
-            return setmetatable(proxy, mt)
+        else
+            -- normal call: base(...)
+            newExpr = {
+                type = "call",
+                base = expr,
+                args = args
+            }
         end
+
+        Logger:add(Compiler.compile(newExpr))
+
+        local proxy = {}
+        Expr.set(proxy, newExpr)
+        return setmetatable(proxy, mt)
     end,
 
     __tostring = function(self)
-        local expr = Expr.get(self)
-        return expr.name or tostring(expr)
+        return Compiler.compile(Expr.get(self))
     end,
 
     __concat = function(a, b)
-        local expression =
-            stringify(a) .. " .. " .. stringify(b)
+        local left  = toExpr(a)
+        local right = toExpr(b)
 
-        Logger:add("CONCAT", expression)
+        local expr = {
+            type  = "binary",
+            op    = "..",
+            left  = left,
+            right = right
+        }
+
+        Logger:add(Compiler.compile(expr))
 
         local proxy = {}
-        Expr.set(proxy, {
-            type = "global",
-            name = "(" .. expression .. ")"
-        })
+        Expr.set(proxy, expr)
+        return setmetatable(proxy, mt)
+    end,
 
+    __len = function(self)
+        local expr = {
+            type    = "len",
+            operand = Expr.get(self)
+        }
+
+        Logger:add(Compiler.compile(expr))
+
+        local proxy = {}
+        Expr.set(proxy, expr)
+        return setmetatable(proxy, mt)
+    end,
+
+    __unm = function(self)
+        local expr = {
+            type    = "unary",
+            op      = "-",
+            operand = Expr.get(self)
+        }
+
+        Logger:add(Compiler.compile(expr))
+
+        local proxy = {}
+        Expr.set(proxy, expr)
         return setmetatable(proxy, mt)
     end,
 }
 
 local function makeProxy(path)
     local object = {}
-
     Expr.set(object, {
         type = "global",
         name = path
     })
-
     return setmetatable(object, mt)
 end
 
 Proxy.new = makeProxy
-Proxy.stringify = stringify
+
+-- keep a public stringify helper for sandbox
+function Proxy.stringify(value)
+    local e = Expr.get(value)
+    if e then
+        return Compiler.compile(e)
+    end
+    if type(value) == "string" then
+        return string.format("%q", value)
+    end
+    return tostring(value)
+end
 
 return Proxy
